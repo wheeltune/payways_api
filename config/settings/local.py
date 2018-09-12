8f1bfa01bb28654da597d913abe62d220c650b9b
wheeltune <wheeltune@gmail.com>
2018-09-12 13:26:00 +0300

    Init commit

---
+++ config/settings/local.py
@@ -0,0 +1,8 @@
+import os
+os.environ["DJANGO_READ_DOT_ENV_FILE"] = "true"
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('DJANGO_SECRET_KEY', default='b-eaqe=c(s79x1f+elyut9^_6xw8jmvz#^d76ot52r87e119j7')
+
+DEBUG = env.bool('DJANGO_DEBUG', default=True)
