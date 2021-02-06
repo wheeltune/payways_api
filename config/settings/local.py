561a0e25144fd67ef96eea0df629102d9c07f836
wheeltune <wheeltune@gmail.com>
2021-02-06 16:01:02 +0300

    Migrate to pipenv

--- config/settings/local.py
+++ config/settings/local.py
@@ -5,8 +5,8 @@ from .base import *
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='b-eaqe=c(s79x1f+elyut9^_6xw8jmvz#^d76ot52r87e119j7')
 
-CORS_ORIGIN_WHITELIST = (
-    'localhost:3000/'
-)
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000'
+]
 
 DEBUG = env.bool('DJANGO_DEBUG', default=True)
